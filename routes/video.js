71b6aaadb17793eafa4a7f0746b8a312ae7e32c1
CodeSprint01 <abuzarzaidi947@gmail.com>
2023-08-11 01:09:47 +0500

    route to api changed

--- routes/video.js
+++ routes/video.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const upload = require('../middlewares/upload-file')
 router.get('/getAll',videoController.getAll);
 // router.get('/getOne/:id',crudController.getOne);
-router.post('/upload',upload.single('video'),videoController.addVideo);
+router.post('/uploadVideo',upload.single('video'),videoController.addVideo);
 // router.delete('/delete/:id',crudController.deleteOne);
 // router.patch('/update/:id',crudController.updateOne);
 
